17ce8b059424badb756319a5026f5bcd4de447ed
tien <milkytien@gmail.com>
2026-03-22 22:12:02 +0800

    Add test echo statement in upload.php

--- upload.php
+++ upload.php
@@ -3,6 +3,7 @@
     $db_handle = new DBController();
     $query = "SELECT DISTINCT department FROM class";
     $result = $db_handle->runQuery($query);
+    echo "test";
 ?>
 <!DOCTYPE html>
 <html lang="en">
